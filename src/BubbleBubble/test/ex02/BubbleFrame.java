63b8f2bd2558ff21a538fc9a5383eb59dc9ea818
songhyun Eon <thgdus79@naver.com>
2024-05-02 14:15:21 +0900

    버블 2단계 완성!

---
+++ src/BubbleBubble/test/ex02/BubbleFrame.java
@@ -0,0 +1,82 @@
+package BubbleBubble.test.ex02;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class BubbleFrame extends JFrame {
+	private JLabel backgroundMap;
+	private Player player;
+
+	public BubbleFrame() {
+		initData();
+		setInitLayout();
+		addEventListener();
+	}
+
+	private void initData() {
+		backgroundMap = new JLabel(new ImageIcon("img/backgroundMap.png"));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		// Frame --> root Panel
+		setContentPane(backgroundMap);
+		setSize(1000, 640);
+
+		player = new Player();
+	}
+
+	private void setInitLayout() {
+		// 좌표값으로 배치
+		setLayout(null);
+		setResizable(false); // 프레임 조절 불가
+		setLocationRelativeTo(null); // JFrame을 여러분 모니터 가운데에 자동 배치
+		setVisible(true);
+
+		add(player);
+	}
+
+	private void addEventListener() {
+		this.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				System.out.println("key code : " + e.getKeyCode());
+
+				switch (e.getKeyCode()) {
+				case KeyEvent.VK_LEFT:
+					player.left();
+					break;
+				case KeyEvent.VK_RIGHT:
+					player.right();
+					break;
+				case KeyEvent.VK_UP:
+					player.up();
+					break;
+				}
+			} // end of key pressed
+			
+			@Override
+			public void keyReleased(KeyEvent e) {
+				switch (e.getKeyCode()) {
+				case KeyEvent.VK_LEFT:
+					player.setLeft(false);
+					break;
+				case KeyEvent.VK_RIGHT:
+					player.setRight(false);
+					break;
+				case KeyEvent.VK_UP:
+					player.setUp(false);
+					break;
+				}
+			} // end of keyReleased
+			
+		}); // end of keyAdapter
+	}
+	
+	// 코드 테스트
+	public static void main(String[] args) {
+		new BubbleFrame();
+	}
+
+}
