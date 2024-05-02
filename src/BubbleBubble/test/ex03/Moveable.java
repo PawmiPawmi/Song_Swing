1dd86d3c659e19ada4bc8c1744730bf2ba101868
songhyun Eon <thgdus79@naver.com>
2024-05-02 15:06:39 +0900

    3단계 완성

---
+++ src/BubbleBubble/test/ex03/Moveable.java
@@ -0,0 +1,14 @@
+package BubbleBubble.test.ex03;
+
+public interface Moveable {
+
+	// public abstract 생략 가능
+	public abstract void left();
+
+	public abstract void right();
+
+	public abstract void up();
+
+	public abstract void down();
+
+}
